6bd03efae63faaff9ff4b04a194e3d63e2e05a77
Christian Schmiedt <120606421+cschm1ed@users.noreply.github.com>
2023-09-13 11:10:33 +0200

    update

--- sources/main.cpp
+++ sources/main.cpp
@@ -1,5 +1,17 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cschmied <cschmied@student.42wolfsburg.d>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/12 13:08:57 by cschmied          #+#    #+#             */
+/*   Updated: 2023/09/12 17:13:02 by cschmied         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include <iostream>
+#include <sys/select.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <colors.hpp>
@@ -7,57 +19,87 @@
 #include <unistd.h>
 #include <cstring>
 #include <arpa/inet.h>
+#include <deque>
 
 #define MAX_CONNECTIONS 5
 #define PORT 8080
 
+static int setFindMax(fd_set &set);
+
+static int setupServerSocket(sockaddr_in &serverSocket, int  &serverFD);
+
 int main() {
-    std::cout << "server startup\n";
+	sockaddr_in serverSocket, clientSocketTmp;
+	int maxFd, serverFd;
+	fd_set incoming, ready;
+	socklen_t clientSocketSize;
+	std::deque<int> clientFds;
+	std::deque <sockaddr_in> clientSockets;
 
-    int clientDescriptor, socketDescriptor = socket(AF_INET, SOCK_STREAM, 0);
+	if (setupServerSocket(serverSocket, serverFd) < 0) {
+		std::cout << RED << "ERROR: setting up server failed\n";
+		return 1;
+	}
+	FD_ZERO(&incoming);
+	FD_SET(serverFd, &incoming);
 
-    if (socketDescriptor < 0) {
-        std::cout << RED << "ERROR: creating socket failed\n" << R;
-        return 1;
-    }
+	while (1) {
+		ready = incoming;
+		maxFd = setFindMax(ready);
 
-    struct sockaddr_in socketAddress, clientSocket;
+		select(maxFd + 1, &ready, NULL, NULL, NULL);
+		for (int fd = 0; fd <= maxFd; fd++) {
+			if (FD_ISSET(fd, &ready)) {
+				if (fd == serverFd) {
+					clientSocketSize = sizeof(clientSocketTmp);
+					int clientDescriptor = accept(serverFd, (struct sockaddr *) &clientSocketTmp, &clientSocketSize);
+					if (clientDescriptor < 0) {
+						perror("accept(): ");
+						return 1;
+					}
+					clientFds.push_back(clientDescriptor);
+					FD_SET(clientDescriptor, &incoming);
+				} else {
+					write(fd, "Hello from Server\n", 19);
+					FD_CLR(fd, &incoming);
+					close(fd);
+					clientFds.erase(std::find(clientFds.begin(), clientFds.end(), fd));
+				}
+			}
+		}
 
-    socketAddress.sin_family = AF_INET;
-    socketAddress.sin_addr.s_addr = INADDR_ANY;
-    socketAddress.sin_port = htons(PORT);
-    
-    if (bind(socketDescriptor, (struct sockaddr *) &socketAddress, sizeof(socketAddress)) < 0) {
-        std::cout << RED << "ERROR: binding socket failed\n " << R;
-        return 1;
-    }
+	}
+	close(serverFd);
+	return 1;
+}
 
-    if (listen(socketDescriptor, MAX_CONNECTIONS) < 0) {
-        std::cout << RED << "ERROR: listening on socket failed\n" << R;
-        return 1;
-    }
+static int setupServerSocket(sockaddr_in &serverSocket, int & serverFd) {
+	serverFd = socket(AF_INET, SOCK_STREAM, 0);
+	socklen_t serverSize = sizeof(serverSocket);
 
-    socklen_t clientLen = sizeof(clientSocket);
-    if ((clientDescriptor = accept(socketDescriptor, (struct sockaddr *) &clientSocket, &clientLen)) < 0) {
-        std::cout << RED << "ERROR: accepting socket failed\n" << R;
-        return 1;
-    }
-    /*
-    std::stringstream incoming;
-    {
-        char c;
-        while (read(clientDescriptor, &c, 1)) {
-            incoming << c;
-        }
-    }
-     */
+	if (serverFd < 0) {
+		perror("socket(): ");
+		return -1;
+	}
+	serverSocket.sin_family = AF_INET;
+	serverSocket.sin_addr.s_addr = htonl(INADDR_ANY);
+	serverSocket.sin_port = htons(PORT);
+	if (bind(serverFd, (struct sockaddr *) &serverSocket, serverSize) < 0) {
+		perror("bind(): ");
+		return -1;
+	}
+	if (listen(serverFd, MAX_CONNECTIONS) < 0) {
+		perror("listen(): ");
+		return -1;
+	}
+	return 0;
+}
 
-
-    std::string outgoing = "Hello from server!\n";
-    const char *buf = outgoing.c_str();
-    write(clientDescriptor, buf, strlen(buf));
-
-    close(clientDescriptor);
-    close(socketDescriptor);
-    return 0;
+static int setFindMax(fd_set &set) {
+	for (int i = FD_SETSIZE; i > 0; --i) {
+		if (FD_ISSET(i, &set)) {
+			return i;
+		}
+	}
+	return 0;
 }
